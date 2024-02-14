15cead8cbe51f7660f426306bb1923981e549305
Gianmarco Ottavi <ottavig91@gmail.com>
2024-02-14 12:27:54 +0100

    Updated performance logging

--- config/riscv/boards/cv32a6/boardsupport.c
+++ config/riscv/boards/cv32a6/boardsupport.c
@@ -8,21 +8,51 @@
    SPDX-License-Identifier: GPL-3.0-or-later */
 
 #include <support.h>
+#include "encoding.h"
+
+#define INHIBIT_MASK 0x1F8
+int cycles, instret, icache_miss, dcache_miss, load_event, store_event, misprediction, stall_issue;
 
 void
 initialise_board ()
 {
-  __asm__ volatile ("li a0, 0" : : : "memory");
+
+  write_csr(0x320, INHIBIT_MASK); // Inhibit the counters.
+  write_csr(mhpmevent3, 1); // l1 I-cache miss
+  write_csr(mhpmevent4, 2); // l1 d-cache miss
+  write_csr(mhpmevent5, 5); // load event
+  write_csr(mhpmevent6, 6); // store event
+  write_csr(mhpmevent7, 10); // branch misprediction
+  write_csr(mhpmevent8, 26); // stall_issue
 }
 
 void __attribute__ ((noinline)) __attribute__ ((externally_visible))
 start_trigger ()
 {
-  __asm__ volatile ("li a0, 0" : : : "memory");
+  icache_miss   = -read_csr(mhpmcounter3);
+  dcache_miss   = -read_csr(mhpmcounter4);
+  load_event    = -read_csr(mhpmcounter5);
+  store_event   = -read_csr(mhpmcounter6);
+  misprediction = -read_csr(mhpmcounter7);
+  stall_issue   = -read_csr(mhpmcounter8);
+  cycles  = -rdcycle();
+  instret = -rdinstret();
+  write_csr(0x320, 0); // Inhibit the counters.
 }
 
 void __attribute__ ((noinline)) __attribute__ ((externally_visible))
 stop_trigger ()
 {
-  __asm__ volatile ("li a0, 0" : : : "memory");
+  write_csr(0x320, INHIBIT_MASK);
+  cycles  += rdcycle();
+  instret += rdinstret();
+  icache_miss   += read_csr(mhpmcounter3);
+  dcache_miss   += read_csr(mhpmcounter4);
+  load_event    += read_csr(mhpmcounter5);
+  store_event   += read_csr(mhpmcounter6);
+  misprediction += read_csr(mhpmcounter7);
+  stall_issue   += read_csr(mhpmcounter8);
+
+  printf("tot_cycles = %d\ninstret = %d\n", cycles, instret);
+  printf("icache_miss = %d\ndcache_miss = %d\nload_event =%d\nstore_event =%d\nmisprediction =%d\nstall_issue =%d\n", icache_miss, dcache_miss, load_event, store_event, misprediction, stall_issue);
 }
--- config/riscv/boards/cv64a6/boardsupport.c
+++ config/riscv/boards/cv64a6/boardsupport.c
@@ -9,24 +9,50 @@
 
 #include <support.h>
 #include "encoding.h"
-int cycles, instret;
+
+#define INHIBIT_MASK 0x1F8
+int cycles, instret, icache_miss, dcache_miss, load_event, store_event, misprediction, stall_issue;
 
 void
 initialise_board ()
 {
+
+  write_csr(0x320, INHIBIT_MASK); // Inhibit the counters.
+  write_csr(mhpmevent3, 1); // l1 I-cache miss
+  write_csr(mhpmevent4, 2); // l1 d-cache miss
+  write_csr(mhpmevent5, 5); // load event
+  write_csr(mhpmevent6, 6); // store event
+  write_csr(mhpmevent7, 10); // branch misprediction
+  write_csr(mhpmevent8, 26); // stall_issue
 }
 
 void __attribute__ ((noinline)) __attribute__ ((externally_visible))
 start_trigger ()
 {
-  cycles  = -read_csr(0xb00);
-  instret = -read_csr(0xb02);
+  icache_miss   = -read_csr(mhpmcounter3);
+  dcache_miss   = -read_csr(mhpmcounter4);
+  load_event    = -read_csr(mhpmcounter5);
+  store_event   = -read_csr(mhpmcounter6);
+  misprediction = -read_csr(mhpmcounter7);
+  stall_issue   = -read_csr(mhpmcounter8);
+  cycles  = -rdcycle();
+  instret = -rdinstret();
+  write_csr(0x320, 0); // Inhibit the counters.
 }
 
 void __attribute__ ((noinline)) __attribute__ ((externally_visible))
 stop_trigger ()
 {
-  cycles  += read_csr(0xb00);
-  instret += read_csr(0xb02);
+  write_csr(0x320, INHIBIT_MASK);
+  cycles  += rdcycle();
+  instret += rdinstret();
+  icache_miss   += read_csr(mhpmcounter3);
+  dcache_miss   += read_csr(mhpmcounter4);
+  load_event    += read_csr(mhpmcounter5);
+  store_event   += read_csr(mhpmcounter6);
+  misprediction += read_csr(mhpmcounter7);
+  stall_issue   += read_csr(mhpmcounter8);
+
   printf("tot_cycles = %d\ninstret = %d\n", cycles, instret);
+  printf("icache_miss = %d\ndcache_miss = %d\nload_event =%d\nstore_event =%d\nmisprediction =%d\nstall_issue =%d\n", icache_miss, dcache_miss, load_event, store_event, misprediction, stall_issue);
 }
